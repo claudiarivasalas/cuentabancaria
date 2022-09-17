fca97143ae7c865e2bdf6edba6ce7d2ddb932007
claudiarivasalas <claudia.rivas.salas@gmail.com>
2022-09-16 23:52:14 -0300

    Cuenta Bancaria

---
+++ bank_account.py
@@ -0,0 +1,40 @@
+class CuentaBancaria:
+    # ¡No olvides agregar algunos valores predeterminados para estos parámetros!
+    nombre_banco = "Primer Dojo Nacional"
+    todas_las_cuentas = []
+
+    def __init__(self, tasa_interes, balance):
+        self.tasa_interes = tasa_interes
+        self.balance = balance
+        CuentaBancaria.todas_las_cuentas.append(self)
+
+    def deposito(self, amount):
+        self.balance += amount
+        return self
+
+    def retiro(self, amount):
+        if self.balance < amount:
+            self.balance -= 5
+            print("Fondos Insuficientes, te restaremos 5")
+        else:
+            self.balance -= amount
+        return self
+
+    def mostrar_info_cuenta(self):
+        print("Balance actual: ", self.balance)
+        return self
+
+    def generar_interes(self):
+        if self.balance > 0:
+            self.balance = self.balance + (self.balance * self.tasa_interes)  
+        return self
+
+    # BONUS NINJA: utiliza un método de clase para imprimir todas las instancias de la información de una cuenta bancaria
+
+    @classmethod
+    def imprime_cuentas_bancarias(cls):
+        sum = 0
+        # utilizamos cls para referirnos a la clase
+        for account in cls.todas_las_cuentas:
+            sum += balance.cuenta
+        return sum
---
+++ test.py
@@ -0,0 +1,12 @@
+from bank_account import CuentaBancaria
+# Crea 2 cuentas
+
+# Para la primera cuenta, haz 3 depósitos y 1 retiro, luego genera intereses y muestra la información de la cuenta, todo en una línea de código (es decir, encadenando)
+
+cuenta1 = CuentaBancaria(0.15, 100)
+cuenta1.deposito(100).deposito(200).deposito(300).retiro(400).generar_interes().mostrar_info_cuenta()
+
+# Para la segunda cuenta, haz 2 depósitos y 4 retiros, luego genera intereses y muestra la información de la cuenta, todo en una línea de código (es decir, encadenando)
+
+cuenta2 = CuentaBancaria(0.15, 100)
+cuenta2.deposito(300).deposito(500).retiro(100).retiro(200).retiro(200).retiro(200).generar_interes().mostrar_info_cuenta()
